8b501013722fd7436acc01820d5d80a01b7c68d4
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2017-11-09 17:42:26 +0200

    webpack config

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    plugins: [
+        require('precss')({ /* ...options */ }),
+        require('autoprefixer')({ browsers: ['last 2 versions'] })
+    ]
+};
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, './public/build'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        modules: ['src', 'node_modules']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: [/node_modules/, /public/],
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', 'stage-0', 'stage-1']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [ 'css-loader', 'postcss-loader', 'resolve-url-loader', 'sass-loader' ]
+                })
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [ 'css-loader', 'postcss-loader' ]
+                })
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css', {
+            allChunks: true
+        })
+    ]
+};
