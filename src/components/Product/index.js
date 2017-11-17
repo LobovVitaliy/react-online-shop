6771940bc46039dfca9b9a98e9cf4d7c4060f1b9
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2017-11-18 01:04:13 +0200

    product (modified)

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './index.scss';
 
-import img from '../../../static/images/img1.jpg';
-
 const Product = (props) => {
     const product = props.product;
     return (
         <div className='product'>
             <div className='product-img-btns'>
-                <img src={img} />
+                <img src={product.img} />
                 <div className='buttons'>
                     <button>Add to bag</button>
                     <button>Buy</button>
