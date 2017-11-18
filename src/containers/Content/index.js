5872da77650c358c9296596221eb484b6e92d84e
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2017-11-18 16:14:08 +0200

    content (static images)

--- src/containers/Content/index.js
+++ src/containers/Content/index.js
@@ -5,31 +5,31 @@ import Card from '../../components/Card';
 
 const cards = [
     {
-        img: require('../../../static/images/1.png'),
+        img: require('../../../static/images/1.jpg'),
         title: 'Apple MacBook Pro',
         text: 'Core i5 - (8 GB/128 GB SSD/OS X Yosemite) MF839...',
         price: '93,990'
     },
     {
-        img: require('../../../static/images/2.png'),
+        img: require('../../../static/images/2.jpg'),
         title: 'Acer Aspire R3',
         text: 'Pentium Quad Core 4th Gen - (4 GB/500 GB HDD/Windo...',
         price: '24,990'
     },
     {
-        img: require('../../../static/images/3.png'),
+        img: require('../../../static/images/3.jpeg'),
         title: 'Micromax Canvas II',
         text: '(WIFI) Atom 4th Gen - (2 GB/32 GB EMMC ...',
         price: '9,999'
     },
     {
-        img: require('../../../static/images/4.png'),
+        img: require('../../../static/images/4.jpg'),
         title: 'Acer One 10 Atom',
         text: '5th Gen - (2 GB/32 GB EMMC Storage/Windows 10 Ho...',
         price: '13,990'
     },
     {
-        img: require('../../../static/images/5.png'),
+        img: require('../../../static/images/5.jpeg'),
         title: 'Acer E15 Celeron',
         text: 'Dual Core 4th Gen - (2 GB/500 GB HDD/Linux) UN.M...',
         price: '15,490'
