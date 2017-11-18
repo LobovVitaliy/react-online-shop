47b122af67a14b298d4e0ed7dbae28d650e1eef2
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2017-11-18 21:35:52 +0200

    footer (simplified)

--- src/containers/Footer/index.js
+++ src/containers/Footer/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './index.scss';
 
-const Footer = (props) => {
-    return (
-        <footer>Copyright 2017. All Rights Reserved</footer>
-    );
-}
+const Footer = () => (
+    <footer>Copyright 2017. All Rights Reserved</footer>
+);
 
 export default Footer;
