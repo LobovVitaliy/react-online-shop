d14a6bbaebb39669f6112823eba903f7bba6447a
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2017-11-20 16:00:38 +0200

    redux (configuration)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 import Site from './containers/Site';
 
-ReactDOM.render(<Site />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <Site />
+    </Provider>,
+    document.getElementById('root')
+);
