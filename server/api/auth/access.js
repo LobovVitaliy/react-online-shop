b5a7545217be0daff10595ce2ff229ef9194f609
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2017-11-27 19:37:26 +0200

    server (passport authenticate)

---
+++ server/api/auth/access.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const passport = require('passport');
+const session = require('../../config/settings').jwt.session;
+
+module.exports = {
+    user: (req, res, next) => {
+        passport.authenticate('user-jwt', { session })(req, res, next);
+    },
+    admin: (req, res, next) => {
+        passport.authenticate('admin-jwt', { session })(req, res, next);
+    }
+};
