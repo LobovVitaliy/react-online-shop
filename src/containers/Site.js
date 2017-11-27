e57adaf4775d9acdce18e119e1cdabea9a643452
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2017-11-27 22:40:21 +0200

    site (modified)

--- src/containers/Site.js
+++ src/containers/Site.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import Header from './Header';
+import Header from '../components/Header';
 import Content from './Content';
-import Footer from './Footer';
+import Footer from '../components/Footer';
 import NotificationBar from './NotificationBar';
 
 class Site extends Component {
