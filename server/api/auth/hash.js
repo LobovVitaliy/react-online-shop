6a2cd11eb995258129ddc23298cdfd7d36a8c89d
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2017-11-27 19:36:34 +0200

    server api (hash password)

---
+++ server/api/auth/hash.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const crypto = require('crypto');
+const { algorithm, salt, digest } = require('../../config/settings').crypto;
+
+module.exports.sha256 = password => (
+    crypto.createHmac(algorithm, salt)
+        .update(password)
+        .digest(digest)
+);
