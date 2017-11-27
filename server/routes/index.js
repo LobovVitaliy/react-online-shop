b1b9cbd44205e2eeca2c901b56c9b127c574ba96
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2017-11-27 19:19:53 +0200

    server (routes)

---
+++ server/routes/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const settings = require('../config/settings');
+
+module.exports = app => {
+    app.options('*', (req, res) => res.send(200));
+
+    app.use('/static', express.static(settings.static));
+    app.use('/api/v1', require('../api'));
+    app.get('*', (req, res) => res.sendFile('index.html', { root: settings.static }));
+    app.use('*', (req, res) => res.status(404).json({ error: 'Not Found' }));
+
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500).json({ error: err.message });
+    });
+};
