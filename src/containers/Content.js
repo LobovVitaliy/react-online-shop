3b84b8d76b8396486f5e78c4dbebf0fd24123e60
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2017-11-28 00:48:25 +0200

    Content (in one file)

--- src/containers/Content.js
+++ src/containers/Content.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import './index.scss';
-
 import { Switch, Redirect, Route } from 'react-router-dom';
 
-import Products from '../Products';
-import Product from '../../components/Product';
-import LoginForm from '../LoginForm';
-import SignUpForm from '../SignUpForm';
-import Cart from '../Cart';
-import NotFound from '../NotFound';
+import Products from './Products';
+import Product from './Product';
+import LoginForm from './LoginForm';
+import SignUpForm from './SignUpForm';
+import NotFound from '../components/NotFound';
 
 class Content extends Component {
     render() {
@@ -20,7 +17,7 @@ class Content extends Component {
                     <Route path='/products/:id' component={Product} />
                     <Route exact path='/login' component={LoginForm} />
                     <Route exact path='/signup' component={SignUpForm} />
-                    <Route exact path='/cart' component={Cart} />
+                    {/* <Route exact path='/cart' component={Cart} /> */}
                     <Route component={NotFound} />
                 </Switch>
             </div>
