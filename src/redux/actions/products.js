b6dc6207add2a4f258ac55eb767a895892882783
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2017-11-29 21:52:43 +0200

    redux (products: updated)

--- src/redux/actions/products.js
+++ src/redux/actions/products.js
@@ -5,8 +5,16 @@ import { notify } from './notification';
 
 export const GET_PRODUCTS = 'GET_PRODUCTS';
 
-export default () => dispatch => {
-    axios.get('/api/v1/products')
-        .then(res => dispatch({ type: GET_PRODUCTS, data: res.data }))
+const success = data => {
+    return {
+        type: GET_PRODUCTS,
+        count: data.count,
+        data: data.products
+    };
+};
+
+export default (page, limit) => dispatch => {
+    axios.get('/api/v1/products', { params: { page, limit } })
+        .then(res => dispatch(success(res.data)))
         .catch(err => dispatch(notify(geterror(err))));
 };
--- src/redux/reducers/products.js
+++ src/redux/reducers/products.js
@@ -1,10 +1,18 @@
 import { GET_PRODUCTS } from '../actions/products';
 
-export default function(state = [], action) {
+const initialState = {
+    count: 0,
+    data: []
+};
+
+export default function(state = initialState, action) {
     switch (action.type) {
 
         case GET_PRODUCTS:
-            return action.data;
+            return {
+                count: action.count,
+                data: action.data
+            };
 
         default:
             return state;
