5d3fa940bb1c48c56d3c7263f181a16497330aa2
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2017-11-29 22:50:49 +0200

    Site (modified)

--- src/containers/Site.js
+++ src/containers/Site.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import Header from '../components/Header';
+import Header from './Header';
 import Content from './Content';
 import Footer from '../components/Footer';
 import NotificationBar from './NotificationBar';
