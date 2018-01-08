c3ca440d0fcc45aad1a9a70c0a08759783aef012
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2018-01-08 21:36:38 +0200

    redux (auth: updated url)

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -9,14 +9,14 @@ export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 
 export const signup = user => dispatch => {
-    axios.post('/api/v1/auth/signup', user)
+    axios.post('/api/v1/users/signup', user)
         .then(() => dispatch(push('/login')))
         .then(() => dispatch(notify('Registration successful!')))
         .catch(err => dispatch(notify(geterror(err))))
 };
 
 export const login = user => dispatch => {
-    axios.post('/api/v1/auth/login', user)
+    axios.post('/api/v1/users/login', user)
         .then(() => dispatch({ type: LOGIN }))
         .then(() => dispatch(push('/cart')))
         .then(() => dispatch(notify('Login successful!')))
