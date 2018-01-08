0ddf21e09a49fe115705115fcabdbd89027f9bd8
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2018-01-08 21:40:26 +0200

    redux (products: added isFetching)

--- src/redux/actions/products.js
+++ src/redux/actions/products.js
@@ -3,17 +3,19 @@ import axios from 'axios';
 import { geterror } from './helper';
 import { notify } from './notification';
 
+export const REQUEST_PRODUCTS = 'REQUEST_PRODUCTS';
 export const GET_PRODUCTS = 'GET_PRODUCTS';
 
-const success = data => {
-    return {
-        type: GET_PRODUCTS,
-        count: data.count,
-        data: data.products
-    };
-};
+const request = () => ({ type: REQUEST_PRODUCTS });
+
+const success = data => ({
+    type: GET_PRODUCTS,
+    count: data.count,
+    data: data.products
+});
 
 export default (page, limit) => dispatch => {
+    dispatch(request());
     axios.get('/api/v1/products', { params: { page, limit } })
         .then(res => dispatch(success(res.data)))
         .catch(err => dispatch(notify(geterror(err))));
--- src/redux/reducers/products.js
+++ src/redux/reducers/products.js
@@ -1,6 +1,7 @@
-import { GET_PRODUCTS } from '../actions/products';
+import { REQUEST_PRODUCTS, GET_PRODUCTS } from '../actions/products';
 
 const initialState = {
+    isFetching: false,
     count: 0,
     data: []
 };
@@ -8,8 +9,15 @@ const initialState = {
 export default function(state = initialState, action) {
     switch (action.type) {
 
+        case REQUEST_PRODUCTS:
+            return {
+                ...state,
+                isFetching: true
+            };
+
         case GET_PRODUCTS:
             return {
+                isFetching: false,
                 count: action.count,
                 data: action.data
             };
