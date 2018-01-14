ed8533293b2820e17ee4c05d9f09d37c10848581
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2018-01-14 14:52:19 +0200

    redux (cart: updated)

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -3,25 +3,39 @@ import axios from 'axios';
 import { geterror } from './helper';
 import { notify } from './notification';
 
-export const CHANGE_CART_ITEMS = 'CHANGE_CART_ITEMS';
+const url = '/api/v1/cart';
 
-const success = items => ({ type: CHANGE_CART_ITEMS, items });
+export const REQUEST_CART = 'REQUEST_CART';
+export const GET_CART_ITEMS = 'GET_CART_ITEMS';
+export const CREATE_CART_ITEM = 'CREATE_CART_ITEM';
+export const DELETE_CART_ITEM = 'DELETE_CART_ITEM';
+
+const request = () => ({ type: REQUEST_CART });
 
 export const get = () => dispatch => {
-    axios.get('/api/v1/cart')
+    const success = data => ({ type: GET_CART_ITEMS, data });
+
+    dispatch(request());
+    axios.get(url)
         .then(res => dispatch(success(res.data)))
         .catch(err => dispatch(notify(geterror(err))));
 };
 
-export const add = id => dispatch => {
-    axios.post('/api/v1/cart', { id })
-        .then(res => dispatch(success(res.data)))
+export const add = item => dispatch => {
+    const success = () => ({ type: CREATE_CART_ITEM, item });
+
+    dispatch(request());
+    axios.post(url, { id: item._id })
+        .then(() => dispatch(success()))
         .then(() => dispatch(notify('Product added!')))
         .catch(err => dispatch(notify(geterror(err))));
 };
 
 export const remove = id => dispatch => {
-    axios.delete(`/api/v1/cart/${id}`)
-        .then(res => dispatch(success(res.data)))
+    const success = () => ({ type: DELETE_CART_ITEM, id });
+
+    dispatch(request());
+    axios.delete(`${url}/${id}`)
+        .then(() => dispatch(success()))
         .catch(err => dispatch(notify(geterror(err))));
 };
--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -1,10 +1,41 @@
-import { CHANGE_CART_ITEMS } from '../actions/cart';
+import {
+    REQUEST_CART,
+    GET_CART_ITEMS,
+    CREATE_CART_ITEM,
+    DELETE_CART_ITEM
+} from '../actions/cart';
 
-export default function(state = [], action) {
+const initialState = {
+    isFetching: false,
+    data: []
+};
+
+export default function(state = initialState, action) {
     switch (action.type) {
 
-        case CHANGE_CART_ITEMS:
-            return action.items;
+        case REQUEST_CART:
+            return {
+                isFetching: true,
+                data: state.data
+            };
+
+        case GET_CART_ITEMS:
+            return {
+                isFetching: false,
+                data: action.data
+            };
+
+        case CREATE_CART_ITEM:
+            return {
+                isFetching: false,
+                data: state.data.concat(action.item)
+            };
+
+        case DELETE_CART_ITEM:
+            return {
+                isFetching: false,
+                data: state.data.filter(item => item._id !== action.id)
+            };
 
         default:
             return state;
