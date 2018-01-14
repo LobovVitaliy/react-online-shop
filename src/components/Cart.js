b2b96f17daf489e883a7f172d5f9110be7d2040f
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2018-01-14 15:02:38 +0200

    Cart (moved and updated)

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import CartItem from '../CartItem';
-import NotFound from '../NotFound';
+import Loading from './Loading';
+import CartItem from './CartItem';
 
 class Cart extends Component {
     componentDidMount() {
@@ -10,17 +10,13 @@ class Cart extends Component {
     }
 
     render() {
-        const { isLoggedIn, items } = this.props;
+        const { isFetching, items, remove } = this.props;
 
-        if (!isLoggedIn) return <NotFound />;
+        if (isFetching) return <Loading />;
         return (
             <div className='col-lg-20'>
                 {items.length ? items.map((item, i) => (
-                    <CartItem
-                        item={item}
-                        remove={this.props.remove}
-                        key={i}
-                    />
+                    <CartItem item={item} remove={remove} key={i} />
                 )) : <h3>The cart is empty</h3>}
             </div>
         );
@@ -28,7 +24,7 @@ class Cart extends Component {
 }
 
 Cart.propTypes = {
-    isLoggedIn: PropTypes.bool.isRequired,
+    isFetching: PropTypes.bool.isRequired,
     items: PropTypes.array.isRequired,
     get: PropTypes.func.isRequired,
     remove: PropTypes.func.isRequired
