c1c04de7b8752c70e17a86bc5e114e4b94d10ed2
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2018-01-14 18:03:18 +0200

    bluebird

--- server/mongodb/index.js
+++ server/mongodb/index.js
@@ -4,6 +4,6 @@ const mongoose = require('mongoose');
 const settings = require('../config/settings');
 
 module.exports = () => {
-    mongoose.Promise = Promise;
+    mongoose.Promise = require('bluebird');
     mongoose.connect(settings.mongo.url, settings.mongo.options);
 };
