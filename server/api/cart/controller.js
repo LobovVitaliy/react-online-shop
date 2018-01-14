cc0f0d3240bd315f59ce55b51ce83e2744747f27
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2018-01-14 14:49:20 +0200

    server api (cart controller: updated)

--- server/api/cart/controller.js
+++ server/api/cart/controller.js
@@ -10,15 +10,12 @@ module.exports = {
             .catch(err => next(err));
     },
     add: (req, res, next) => {
-        const user = req.user;
-        const id = req.body.id;
+        const { user, body: { id } } = req;
 
-        if (user.cart.indexOf(id) === -1) {
-            Product.findById(id)
-                .then(() => user.cart.push(id))
-                .then(() => user.save())
-                .then(() => Product.find({ _id: { $in: user.cart } }))
-                .then(products => res.json(products))
+        if (!user.cart.includes(id)) {
+            user.cart.push(id);
+            user.save()
+                .then(() => res.sendStatus(200))
                 .catch(err => next(err));
         } else {
             next(AppError.badRequest('Product was already added'));
@@ -31,8 +28,7 @@ module.exports = {
         if (index !== -1) {
             user.cart.splice(index, 1);
             user.save()
-                .then(() => Product.find({ _id: { $in: user.cart } }))
-                .then(products => res.json(products))
+                .then(() => res.sendStatus(200))
                 .catch(err => next(err));
         } else {
             next(AppError.badRequest('Product Not Found'));
